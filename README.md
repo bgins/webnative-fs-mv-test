5c092f0b969fc428485f0971756f29937e9b8fe8
Brian Ginsburg <gins@brianginsburg.com>
2023-02-01 16:28:11 -0800

    Add content check

--- README.md
+++ README.md
@@ -19,3 +19,12 @@ npm run dev
 ```
 
 Open the devtools to check for console messages then click the "Run test" button.
+
+On first run, this will:
+
+1. Register a user
+2. Add a file to `dirA`
+3. Move the file to `dirB`
+4. Read the file from `dirB`
+
+On subsequent runs it will attempt to read the content from `dirB`.
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Webnative fs.mv test</h1>
     <div class="card">
-      <button id="run-test" type="button">Run test</button>
+      <button id="run-test" type="button">Run Test</button>
     </div>
   </div>
 `
@@ -18,14 +18,13 @@ if (testButton) {
 
 async function runTest() {
   const program = await webnative.program({
-    namespace: { creator: "bgins", name: "webnative-mv-test" }
+    namespace: { creator: "bgins", name: "webnative-mv-test" },
+    debug: true
   })
 
   if (program.session) {
-    console.log("active session")
-
     if (program.session.fs) {
-      await testFileSystem(program.session.fs)
+      await readContent(program.session.fs)
     } else {
       console.warn("No file system :(")
     }
@@ -38,7 +37,7 @@ async function runTest() {
 
     if (session) {
       if (session.fs) {
-        await testFileSystem(session.fs)
+        await writeAndMoveContent(session.fs)
       } else {
         console.warn("No file system :(")
       }
@@ -46,12 +45,16 @@ async function runTest() {
   }
 }
 
-async function testFileSystem(fs: webnative.FileSystem) {
-  const dirA = webnative.path.directory("private", "dirA")
-  const dirB = webnative.path.directory("private", "dirB")
-  const filePath = webnative.path.file("hello.txt")
+// File system
 
+const dirA = webnative.path.directory("private", "dirA")
+const dirB = webnative.path.directory("private", "dirB")
+const filePath = webnative.path.file("hello.txt")
+
+async function writeAndMoveContent(fs: webnative.FileSystem) {
   // Write a file to dirA
+  console.log("Adding a file to dirA")
+
   await fs.write(
     webnative.path.combine(dirA, filePath),
     new TextEncoder().encode("👋")
@@ -59,8 +62,10 @@ async function testFileSystem(fs: webnative.FileSystem) {
   await fs.publish()
 
   // Move the file to dirB
+  console.log("Moving the file to dirB")
+
   await fs.mv(
-    webnative.path.combine(dirA, filePath), 
+    webnative.path.combine(dirA, filePath),
     webnative.path.combine(dirB, filePath)
   )
   await fs.publish()
@@ -70,5 +75,14 @@ async function testFileSystem(fs: webnative.FileSystem) {
     // @ts-ignore
     await fs.read(webnative.path.combine(dirB, filePath))
   )
-  console.log("Content in dirB: ", content)
+  console.log("Content in dirB after the move: ", content)
+}
+
+async function readContent(fs: webnative.FileSystem) {
+  // Read the file from dirB
+  const content = new TextDecoder().decode(
+    // @ts-ignore
+    await fs.read(webnative.path.combine(dirB, filePath))
+  )
+  console.log("Content in dirB on content check: ", content)
 }
